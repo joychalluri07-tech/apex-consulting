2ac181bc8897004599cf5d10b7f02ec51227c8e9
joychalluri07-tech <joychalluri07@gmail.com>
2026-07-16 22:01:10 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    // Replace the link inside the quotes with your real Calendly link!
+    const myCalendlyLink = "https://calendly.com/joychalluri07/30min"; 
+    
+    window.open(myCalendlyLink, "_blank");
+}
